55f4a6a919eaa981849ca742431f186b87086e16
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-06 17:40:05 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicVariables
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string characterName = "Richard";
+            int characterAge = 23;
+            string characterLocation = "Christchurch";
+            char favouriteLetter = 'A';
+            
+            Console.WriteLine(characterName + " is from " + characterLocation + " and is " + characterAge + " and his favourite letter is " + favouriteLetter);
+
+            Console.WriteLine("Every year " + characterName + " leaves his home of " + characterLocation + " to find things that start with the letter " + favouriteLetter);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
